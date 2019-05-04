79fd67e0fe0cdb3ae8fe67822ed3718e6e67d884
potykion <potykion@gmail.com>
2019-05-04 12:31:19 +0300

    list_zip_files: bytes, file attachment support

--- docci/zip.py
+++ docci/zip.py
@@ -1,14 +1,16 @@
 """
 Utils for working with zip archives
 """
-
+import io
 from typing import Union, Iterable, Sequence
 from zipfile import ZipFile
 
 from docci.file import FileAttachment
 
 
-def list_zip_files(zip_file: Union[str, ZipFile]) -> Sequence[FileAttachment]:
+def list_zip_files(
+    zip_file: Union[str, io.BytesIO, ZipFile, FileAttachment]
+) -> Sequence[FileAttachment]:
     """
     List zip archive files
     """
@@ -18,7 +20,10 @@ def list_zip_files(zip_file: Union[str, ZipFile]) -> Sequence[FileAttachment]:
             content = zip_file.read(filename)
             yield FileAttachment(filename, content)
 
-    if isinstance(zip_file, str):
+    if isinstance(zip_file, FileAttachment):
+        zip_file = ZipFile(io.BytesIO(zip_file.content))
+
+    if isinstance(zip_file, (str, io.BytesIO)):
         zip_file = ZipFile(zip_file)
 
     return list(zip_file_generator(zip_file))
--- tests/test_zip.py
+++ tests/test_zip.py
@@ -9,3 +9,13 @@ def test_list_zip_files_show_zip_content() -> None:
     zip_files = list_zip_files(os.path.join(TEST_DATA_DIR, "response.zip"))
     zip_file_names = list(map(attrgetter("name"), zip_files))
     assert zip_file_names == ["PacketDescription.xml", "2f16e9ce590046abac1fd43479845c17.bin"]
+
+
+def test_list_zip_files_show_zip_content_if_zip_is_inside_of_another_zip() -> None:
+    zip_files = list_zip_files(os.path.join(TEST_DATA_DIR, "response.zip"))
+    zip_files_map = dict(zip(map(attrgetter("name"), zip_files), zip_files))
+
+    zip_files = list_zip_files(zip_files_map["2f16e9ce590046abac1fd43479845c17.bin"])
+    zip_file_names = list(map(attrgetter("name"), zip_files))
+
+    assert zip_file_names == ["file"]
